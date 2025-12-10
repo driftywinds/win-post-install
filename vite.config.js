9cf334b3cc05c86e028ae9a54f42eb78fbed8345
drifty <github@drifty.win>
2025-12-10 11:34:51 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/win-post-install/',
+  base: process.env.VITE_BASE_PATH || '/win-post-install/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
